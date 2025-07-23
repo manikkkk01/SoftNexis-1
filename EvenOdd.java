9465c1d979b5cba9fa317cd1a982dcec1e85156e
manikkkk01 <manikkaushik099@gmail.com>
2025-07-23 08:41:47 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,7 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 7;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(number + " is " + result);
+    }
+}
